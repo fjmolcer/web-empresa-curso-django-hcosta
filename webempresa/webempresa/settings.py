6233a3254fbc19756b3ef207425c9d668d2fdd89
Francisco José Molina Cerezo <fjmolinacerezo@gmail.com>
2021-01-07 12:25:13 +0100

    He añadido un template para cambiar la cabecera del panel de administrador

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'webempresa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
